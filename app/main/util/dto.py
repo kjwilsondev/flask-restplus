d2f524d28185422543cd86d1322308c80e2c46d3
KJ Wilson <yourboykjdev@gmail.com>
2019-11-27 22:48:23 -0800

    needed this

---
+++ app/main/util/dto.py
@@ -0,0 +1,14 @@
+# Data transfer object (DTO)
+# Responsible for carrying data between processes
+# Used for marshaling data for our API calls
+
+from flask_restplus import Namespace, fields
+
+class UserDto:
+    api = Namespace('user', description='user related operations')
+    user = api.model('user', {
+        'email': fields.String(required=True, description='user email address'),
+        'username': fields.String(required=True, description='user username'),
+        'password': fields.String(required=True, description='user password'),
+        'public_id': fields.String(description='user Identifier')
+    })

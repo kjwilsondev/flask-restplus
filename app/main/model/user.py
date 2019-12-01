f443f875d1c36650ea14b2d0a001cce2dcf16f7b
KJ Wilson <yourboykjdev@gmail.com>
2019-12-01 15:05:42 -0800

    added functions in user model for encoding and decoding JWTS

--- app/main/model/user.py
+++ app/main/model/user.py
@@ -1,3 +1,9 @@
+# token imports
+import datetime
+import jwt
+from app.main.model.blacklist import BlacklistToken
+from ..config import key
+
 from .. import db, flask_bcrypt
 
 # User class inherits from db.Model class which declares the class as a model for sqlalchemy
@@ -30,3 +36,43 @@ class User(db.Model):
 
     def __repr__(self):
         return "<User '{}'>".format(self.username)
+    
+    @staticmethod
+    def encode_auth_token(self, user_id):
+        """
+        Generates the Auth Token
+        :return: string
+        """
+        try:
+            payload = {
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(days=1, seconds=5),
+                'iat': datetime.datetime.utcnow(),
+                'sub': user_id
+            }
+            return jwt.encode(
+                payload,
+                key,
+                algorithm='HS256'
+            )
+        except Exception as e:
+            return e
+
+    @staticmethod  
+    def decode_auth_token(auth_token):
+        """
+        Decodes the auth token
+        :param auth_token:
+        :return: integer|string
+        """
+        try:
+            payload = jwt.decode(auth_token, key)
+            is_blacklisted_token = BlacklistToken.check_blacklist(auth_token)
+            if is_blacklisted_token:
+                return 'Token blacklisted. Please log in again.'
+            else:
+                # returns user_id
+                return payload['sub']
+        except jwt.ExpiredSignatureError:
+            return 'Signature expired. Please log in again.'
+        except jwt.InvalidTokenError:
+            return 'Invalid token. Please log in again.'

4a4f51c4a3bedd1ccb5c12269c9f54c7458edc2e
KJ Wilson <yourboykjdev@gmail.com>
2019-12-02 05:03:46 -0800

    forgot to import decorator func 🎉

--- app/main/controller/user_controller.py
+++ app/main/controller/user_controller.py
@@ -2,6 +2,7 @@ from flask import request
 from flask_restplus import Resource
 
 from ..util.dto import UserDto
+from ..util.decorator import token_required, admin_token_required
 from ..service.user_service import save_new_user, get_all_users, get_a_user
 from functools import wraps
 
